150af2063a0816d35ce2270357e44db215604d24
Zac <a19860101@gmail.com>
2018-10-29 19:30:34 +0800

    no msg

--- list2.php
+++ list2.php
@@ -1,14 +1,11 @@
-<?php 
+<?php
     require_once("database.php");
-
     $sql = "SELECT * FROM `students`";
     $result = mysqli_query($conn,$sql);
-
-    while($row = mysqli_fetch_assoc($result)){
-        $sts[] = $row; 
+    while($rows = mysqli_fetch_assoc($result)){
+        $row[] = $rows;
+        // echo $rows["name"]."<br>";
     }
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,22 +14,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <style>
-        table,td,th {
-            border-collapse: collapse;
-            border: 1px solid #333;
-            padding: 10px
-        }
-    </style>
 </head>
 <body>
-    <a href="form.php">新增</a>
-    <div>
-        <?php 
-            foreach($sts as $st){
-                echo $st["name"];
+    <ul>
+        <?php
+            foreach($row as $r){
+                echo "<li>".$r["mail"]."</li>";
+            }
+            foreach($row as $r){
+                echo "<li>".$r["name"]."</li>";
+            }
+            foreach($row as $r){
+                echo "<li>".$r["edu"]."</li>";
             }
         ?>
-    </div>
-</body>
+    </ul>
+</body> 
 </html>
