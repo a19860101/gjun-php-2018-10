d058c768c3203c0801fc8b759122f3ba446763d0
Zac <a19860101@gmail.com>
2018-12-17 21:43:21 +0800

    cart

--- shoppingCart/addToCart.php
+++ shoppingCart/addToCart.php
@@ -6,10 +6,12 @@
         $items = $items.",".$_GET["id"];
         $_SESSION["CART"] = $items;
         // echo $_SESSION["CART"];
+        header("location:index.php?status=success");
     }else{
         $items = $_GET["id"];
-        $_SESSION["CART"] = $items;
+        $_SESSION["CART"] = $items; 
         // echo $_SESSION["CART"];
+        header("location:index.php?status=success");
     }
     // session_destroy();
 
--- shoppingCart/cart.php
+++ shoppingCart/cart.php
@@ -13,14 +13,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        table,td,th {
+            border-collapse: collapse;
+            border:1px solid #aaa;
+            padding: 15px;
+            margin: auto;
+        }
+    </style>
 </head>
 <body>
+        <div class="container">
+            <a href="index.php">繼續購物</a>
+            <a href="#">清空購物車</a>
+        </div>
     <table>
         <tr>
             <th>編號</th>
             <th>品名</th>
             <th>價格</th>
-            <th>動作</th>
+            <!-- <th>動作</th> -->
         </tr>
         <?php
             // echo $_SESSION["CART"];
@@ -30,7 +42,15 @@
             $cart_array = explode(",",$cart_string);
             // var_dump($cart_array);
             foreach($cart_array as $items){
-                echo $items;
+                $products = new Products();
+                $product = $products->show($items);
+                foreach($product as $p){
+                    echo "<tr>";
+                    echo "<td>".$p["id"]."</td>";
+                    echo "<td>".$p["title"]."</td>";
+                    echo "<td>".$p["price"]."</td>";
+                    echo "</tr>";
+                }
             }
         ?>
     </table>
