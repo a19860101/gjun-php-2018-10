7cb41b87c0040ad0f7b092e4be66f66c4cfa27e7
Zac <a19860101@gmail.com>
2018-12-17 21:09:02 +0800

    cart

--- shoppingCart/addToCart.php
+++ shoppingCart/addToCart.php
@@ -1,11 +1,15 @@
 <?php
     session_start();
     if(isset($_SESSION["CART"]) && !empty($_SESSION["CART"])) {
-        echo "購物車中有商品";
-        var_dump($_SESSION["CART"]); 
+        // echo $_SESSION["CART"];
+        $items = $_SESSION["CART"];
+        $items = $items.",".$_GET["id"];
+        $_SESSION["CART"] = $items;
+        echo $_SESSION["CART"];
     }else{
         $items = $_GET["id"];
         $_SESSION["CART"] = $items;
         echo $_SESSION["CART"];
     }
+    // session_destroy();
 
